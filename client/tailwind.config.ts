a278b9c66daf0eec0ae55d3d3d2536f4ebdbeae3
shocker-lov-t <tapaslov@gmail.com>
2025-04-02 02:19:32 +0530

    Initial commit with .gitignore

---
+++ client/tailwind.config.ts
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{jsx,tsx}", "./*.html"],
+    theme: {
+        extend: {
+            colors: {
+                dark: "#2d323c",
+                darkHover: "#564D60",
+                light: "#f5f5f5",
+                primary: "#A963E2",
+                danger: "#ef4444",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            animation: {
+                "up-down": "up-down 2s ease-in-out infinite alternate",
+            },
+        },
+    },
+    plugins: [],
+}
